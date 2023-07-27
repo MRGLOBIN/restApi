561cf9070d4005a40d6eb56956b935c0370b2c5c
Muhammad Asim <mrgoblin@Muhammads-MacBook-Pro.local>
2023-07-27 20:17:39 +0500

    encryption done throught bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose')
+const bcrypt = requrie('bcrypt')
+const config = require('config')
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true},
-    name: { type: String, required: true},
-    password: { type: String, required: true}
-}, {
-    timestamps: true
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+userSchema.pre('save', async (next) => {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  const salt = await bcrypt.genSalt(config.get('saltRounds'))
+  const hash = await bcrypt.hash(this.password, salt)
+
+  this.password = hash
+
+  return next()
 })
-
 
 const user = mongoose.model('user', userSchema)
 
